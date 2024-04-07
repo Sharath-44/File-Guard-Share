50298735a1f5c2ca6f85663da3b38803f16a80f2
Shamith <chandrashamith@gmail.com>
2024-04-08 03:55:09 +0530

    Added Peer-2 skeleton as well

---
+++ PEER-2/src/main/java/com/filesharingsystems/filesharingsytemapplication.java
@@ -0,0 +1,13 @@
+package com.filesharingsystems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Peer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Peer1Application.class, args);
+	}
+
+}
---
+++ PEER-2/src/test/java/com/filesharingsystems/Peer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filesharingsystems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Peer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
